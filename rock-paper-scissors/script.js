632e79fcdcce53df576af5eebc1fcbba7d6b2eea
duttrohan0302 <duttrohan0302@gmail.com>
2022-08-06 22:23:56 +0530

    display selection

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -21,9 +21,6 @@ let gameActive = false;
 
 
 // main functions
-const gameFlow = id => {
-
-}
 
 const displayBoards = () => {
 
@@ -37,9 +34,77 @@ const displayBoards = () => {
     select.style.display = "block";
 
     gameActive = true;
-    
+
 }
 
+const computerPlay = () => {
+    let options = ["rock", "paper", "scissors"];
+
+    let randomChoice = options[Math.floor(Math.random()*options.length)];
+
+    return randomChoice;
+}
+
+const playRound = (ps, cs) => {
+
+    // return 0 if it's a draw
+    // return 1 if player won
+    //  return -1 if computer won
+    if(ps===cs){
+        return 0;
+    }else if(ps === "rock" && cs === "scissors"){
+        return 1;
+    }else if(ps === "rock" && cs === "paper"){
+        return -1;
+    }else if(cs === "rock" && ps === "scissors"){
+        return -1;
+    }else if(cs === "rock" && ps === "paper"){
+        return 1;
+    }else if(ps === "scissors" && cs === "paper"){
+        return 1;
+    }else if(ps === "paper" && cs === "scissors"){
+        return -1;
+    }
+
+    // if(ps===cs){
+    //     return 0;
+    // }else if(ps==="rock"){
+    //     return (cs==="scissors" ? 1 : -1)
+    // }else if()
+
+}
+
+const getMeWinner = playerSelection => {
+    const computerSelection = computerPlay();
+
+    let winner = playRound(playerSelection,computerSelection);
+
+    winner = winner === 0 ? "Draw!" : (winnner===1 ? "Player won!" : "Computer won!");
+
+    return ({
+        winner: winner,
+        compMove: computerSelection
+    })
+}
+
+
+const gameFlow = (playerSelection) => {
+    const winnerObject = getMeWinner(playerSelection);
+
+    const result = winnerObject.winner;
+
+    // TODO => COMPLETE THIS FUNC
+    const computerMove = winnerObject.compMove;
+
+    displaySelection('player', playerSelection, results);
+    displaySelection('computer', computerMove, results);
+
+    scoreBoard(result)
+
+    message.innerText = result;
+
+    whoWon()
+}
 
 
 // event listeners
